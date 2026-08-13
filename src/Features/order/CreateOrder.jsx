5a9d96e69c4e1fcb6be1231b9e45bf3824f2f8cf
Your Name <leulgebremariam1995@gmail.com>
2026-08-13 14:52:15 +0300

    feat:using @apply to reuse component designs

--- src/Features/order/CreateOrder.jsx
+++ src/Features/order/CreateOrder.jsx
@@ -50,7 +50,7 @@ const formErrors=useActionData();
       <Form method="POST" >
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type="text" name="customer" className="input" required />
         </div>
 
         <div>
@@ -58,12 +58,7 @@ const formErrors=useActionData();
           <div> 
             <input type="tel" name="phone" 
             
-               className="rounded-full border
-        border-stone-200 px-4 py-2 text-sm
-  transition-all duration-300 placeholder:text-stone-400
-  focus:outline-none focus:ring focus:ring-yellow-400      
-        w-full
-            md:px-6 md:py-3"
+               className="input"
             
             required />
           </div>
@@ -75,13 +70,9 @@ const formErrors=useActionData();
           <div>
             <input type="text" name="address" required 
             
-               className="rounded-full border
-        border-stone-200 px-4 py-2 text-sm
-  transition-all duration-300 placeholder:text-stone-400
-  focus:outline-none focus:ring focus:ring-yellow-400      
-        w-full
-            md:px-6 md:py-3"
-            />
+               className="input"
+            
+          />
           </div>
         </div>
 
