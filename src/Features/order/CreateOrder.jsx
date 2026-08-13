e75d2172ac0ec4cf74f65c84fc83462f48d34eb5
Your Name <leulgebremariam1995@gmail.com>
2026-08-13 13:05:14 +0300

    feat(ui):styling the order now button

--- src/Features/order/CreateOrder.jsx
+++ src/Features/order/CreateOrder.jsx
@@ -81,7 +81,22 @@ const formErrors=useActionData();
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button disabled={true}
+          className="
+          inline-block rounded-full
+          bg-yellow-400 px-4 py-3 font-semibold
+          uppercase tracking-wide text-stone-800 transition-colors 
+          duration-300 hover:bg-yellow-300 
+          focus:bg-yellow-300 
+          focus:outline-none focus:ring
+          focus:ring-offset-2
+          focus:ring-yellow-300
+          disabled:cursor-not-allowed
+        active:bg-yellow-900  
+        "
+          
+          
+          >
             {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
